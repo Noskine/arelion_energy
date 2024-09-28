f6f651fee2002cd6beba0b6b9f2fd1cb150e871c
Noskine <noskine@outlook.com>
2024-09-27 23:50:49 -0300

    finish header menubar

--- src/components/+Header.svelte
+++ src/components/+Header.svelte
@@ -3,9 +3,7 @@
 </script>
 
 <header>
-	<section>
-		<img class="logotype" {src} alt="logotype alternative" />
-	</section>
+	<img class="logotype" {src} alt="logotype alternative" />
 	<menu>
 		<ul>
 			<li><a href="/">Sobre nós</a></li>
@@ -14,34 +12,70 @@
 			<li><a href="/">Simulação de economia</a></li>
 		</ul>
 	</menu>
+	<button id="contactUs"> Entre em contato </button>
 </header>
 
 <style>
 	header {
-        line-height: 10px;
 		padding-inline: 5%;
+		padding-block: 20px;
 		display: flex;
-		align-items: center;
 		justify-content: space-between;
+		align-items: center;
+		background-color: #fff;
+		box-shadow: var(--shadow);
 
+		& menu {
+			padding: 0;
 
-		& menu ul {
-			list-style: none;
-			display: flex;
-			align-items: center;
-			gap: 10px;
+			& ul {
+				list-style: none;
+				display: flex;
+				align-items: center;
+				gap: 15px;
 
-			& li {
+				& li {
+					& > a {
+						font-size: 1.4rem;
+						text-decoration: none;
+						text-transform: capitalize;
+						font-weight: 600;
+						color: var(--color);
+						transition: all 200ms;
 
-                & > a {
-					font-size: 1.4rem;
-                    text-decoration: none;
+						&:hover {
+							color: var(--opaciteColor);
+						}
+					}
 				}
 			}
 		}
 	}
 
 	.logotype {
-		width: 120px;
+		width: 150px;
+		padding: 0;
+	}
+
+	#contactUs {
+		border: none;
+		font-size: 1.6rem;
+		padding: 10px 15px;
+		border-radius: 27px;
+		color: #fff;
+		font-weight: 700;
+		cursor: pointer;
+
+		background: rgb(77, 137, 255);
+		background: -moz-linear-gradient(228deg, rgba(77, 137, 255, 1) 0%, rgba(98, 246, 235, 1) 100%);
+		background: -webkit-linear-gradient(
+			228deg,
+			rgba(77, 137, 255, 1) 0%,
+			rgba(98, 246, 235, 1) 100%
+		);
+		background: linear-gradient(228deg, rgba(77, 137, 255, 1) 0%, rgba(98, 246, 235, 1) 100%);
+		transition: 10s ease-in-out;
+
+		box-shadow: 0px 1px 4px 1px #00000026;
 	}
 </style>
